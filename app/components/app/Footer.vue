dca7eff4565f1e37882d0491aec35b0f06870a4a
MatiasRoje <rojechi@gmail.com>
2025-07-22 22:56:03 +0200

    add github to footer

--- app/components/app/Footer.vue
+++ app/components/app/Footer.vue
@@ -1,5 +1,5 @@
 <template>
   <footer class="text-center mt-auto p-4 bg-zinc-800 text-zinc-50">
-    <p>Copyright &copy; {{ new Date().getFullYear() }}</p>
+    <p>&copy; 2025 by <ULink href="https://github.com/MatiasRoje" target="_blank" class="hover:cursor-pointer text-zinc-50 hover:text-zinc-200">Matías Roje</ULink></p>
   </footer>
 </template>
